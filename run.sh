f20e9dd35bb986fa1a07f2c9b6e47e55128a949c
hellod035 <hellod035@163.com>
2024-10-04 02:15:38 +0800

    init

---
+++ run.sh
@@ -0,0 +1,6 @@
+cd ~/humanoid/legged_gym
+conda activate humanoid
+tensorboard --logdir .
+
+python legged_gym/scripts/train.py --headless
+python legged_gym/scripts/play.py --load_run 0001-locomotion --checkpoint 1000
