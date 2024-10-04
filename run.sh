e102c99884460ba296ac830bcb2e8538effff0d3
hellod035 <hellod035@163.com>
2024-10-04 23:44:38 +0800

    1

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ conda activate humanoid
 tensorboard --logdir .
 
 python legged_gym/scripts/train.py --task h1_flat --headless
-python legged_gym/scripts/play.py --task h1_flat --load_run xxxx --checkpoint 1000
+python legged_gym/scripts/play.py --task h1_flat --load_run xxxx --checkpoint xxxx
 
 
 python legged_gym/scripts/train.py --task h1_rough --headless
-python legged_gym/scripts/play.py --task h1_rough --load_run xxxx --checkpoint 1000
+python legged_gym/scripts/play.py --task h1_rough --load_run xxxx --checkpoint xxxx
