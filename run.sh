e84d90775ab070e804b8c2f8ba0d7ec2d10b727e
Hellod035 <hellod035@163.com>
2024-10-11 23:41:22 +0800

    remove rough

--- run.sh
+++ run.sh
@@ -4,7 +4,3 @@ tensorboard --logdir .
 
 python legged_gym/scripts/train.py --task h1_flat --headless
 python legged_gym/scripts/play.py --task h1_flat --load_run Oct05_01-37-34_ --checkpoint 800
-
-
-python legged_gym/scripts/train.py --task h1_rough --headless
-python legged_gym/scripts/play.py --task h1_rough --load_run xxxx --checkpoint xxxx
